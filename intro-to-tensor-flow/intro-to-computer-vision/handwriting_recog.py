0c8b7ab0362ff52a231a02afdd81b0679dc42031
ashriram-github <ashriram.edu@gmail.com>
2021-07-07 23:34:23 -0400

    handwriting recognition

---
+++ intro-to-tensor-flow/intro-to-computer-vision/handwriting_recog.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from os import path, getcwd, chdir
+
+# DO NOT CHANGE THE LINE BELOW. If you are developing in a local
+# environment, then grab mnist.npz from the Coursera Jupyter Notebook
+# and place it inside a local folder and edit the path to that location
+path = '/Users/anishshriram/Downloads/mnist.npz'
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.99):
+      print("\nReached 99% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+
+def train_mnist():
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path=path)
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    callbacks = myCallback()
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
+
+    return history.epoch, history.history['acc'][-1]
+
+
+train_mnist()
